cc1526e29eb457653a7d583beb1a298befb7eadb
LeeWelch <146404478+LeeWelch@users.noreply.github.com>
2024-06-11 23:49:17 +0000

    adds inventions

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import InventionListView, InventionDetailView
+
 # single . means current folder
 # .. goes up a level
 
@@ -9,6 +11,11 @@ urlpatterns = [
   path("gallery",views.gallery, name="gallery"),
   path("contact",views.contact, name="contact"),
   path('function/', views.function_view, name='function_view'),
-  path('class/', views.ClassView.as_view(), name='class_view')
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('theme/', views.ThemeView.as_view(), name='theme'),
+  path('load/', views.load_default_data_view, name='load_default_data'),
+  path('inventions/', InventionListView.as_view(), name='invention-list'),
+  path('invention/<int:pk>/', InventionDetailView.as_view(), name='invention-view'),
+
 
 ]
